d808258ed16489c80b5de8e108ea7ecdcaa90728
MertDenizdelen <32333129+MertDenizdelen@users.noreply.github.com>
2019-04-03 16:12:14 +0300

    Proje Eklendi

---
+++ BasitFormUygulaması/BasitFormUygulaması/BasitFormUygulaması/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BasitFormUygulaması.MvcApplication" Language="C#" %>
---
+++ BasitFormUygulaması/BasitFormUygulaması/BasitFormUygulaması/Models/FormModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BasitFormUygulaması.Models
+{
+    public class FormModel
+    {
+        [Required(ErrorMessage = "AdıSoyadı gereklidir!!!")]
+        public string AdiSoyadi { get; set; }
+        [Required (ErrorMessage = "Boş Geçilemez")]
+        [EmailAddress(ErrorMessage = "Email Geçersiz")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Boş Olmaz!!!")]
+        [MaxLength(500,ErrorMessage = "500 karateri geçmeyin")]
+        public string Mesaj { get; set; }
+
+        // Eger Kayıt Edilecekse ve mail gönderene geri dönüş olacaksa
+        //public DateTime Tarih { get; set; }
+
+    }
+}
